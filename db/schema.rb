a2a8f1263a7a468a3144c7265290af7aec124060
Dmitrii <veter.666@list.ru>
2018-09-03 17:11:46 +1000

    Исправление замечаний по Интерфейc запросов AvtiveRecord

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 2018_09_01_041109) do
     t.boolean "correct", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "questions_id", null: false
-    t.index ["questions_id"], name: "index_answers_on_questions_id"
+    t.integer "question_id", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2018_09_01_041109) do
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
+  create_table "test_passages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
+  end
+
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 1, null: false
@@ -42,15 +51,6 @@ ActiveRecord::Schema.define(version: 2018_09_01_041109) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
-  end
-
-  create_table "user_profiles", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["test_id"], name: "index_user_profiles_on_test_id"
-    t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,48 +8,46 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+categories = Category.create!([{ title: 'biology' },
+                               { title: 'chemistry' },
+                               { title: 'mathematics' },
+                               { title: 'physics' }])
 
-Category.delete_all
-Category.create([{id: 1, title: 'biology'}, 
-                 {id: 2, title: 'chemistry'}, 
-                 {id: 3, title: 'mathematics'}, 
-                 {id: 4, title: 'physics'}])
+test_ids = Test.create!([{ title: 'botany', level: 1, category: categories[0] },
+                         { title: 'zoology', level: 2, category: categories[0] },
+                         { title: 'mechanics', level: 3, category: categories[3] },
+                         { title: 'optics', level: 2, category: categories[3] },
+                         { title: 'arithmetic', level: 1, category: categories[2] }]).map(&:id)
 
-Test.delete_all
-Test.create([{id: 1, title: 'botany', level: 1, category_id: 1},
-             {id: 2, title: 'zoology', level: 2, category_id: 1},
-             {id: 3, title: 'mechanics', level: 3, category_id: 4},
-             {id: 4, title: 'optics', level: 2, category_id: 4},
-             {id: 5, title: 'arithmetic', level: 1, category_id: 3}])
+qstn_ids = Question.create!([{ body: 'Is a rose a tree?', test_id: test_ids[0] },
+                             { body: 'Is a beetle a mammal?', test_id: test_ids[1] },
+                             { body: 'Who is the largest animal?', test_id: test_ids[1] },
+                             { body: '2 + 3 =', test_id: test_ids[4] },
+                             { body: '2 * 6 =', test_id: test_ids[4] }]).map(&:id)
 
-Question.delete_all
-Question.create([{id: 1, body: 'Is a rose a tree?', test_id: 1},
-                 {id: 2, body: 'Is a beetle a mammal?', test_id: 2},
-                 {id: 3, body: 'Who is the largest animal?', test_id: 2},
-                 {id: 4, body: '2 + 3 =', test_id: 5},
-                 {id: 5, body: '2 * 6 =', test_id: 5}])
+Answer.create!([{ question_id: qstn_ids[0], body: 'No', correct: true },
+                { question_id: qstn_ids[0], body: 'Yes' },
+                { question_id: qstn_ids[1], body: 'No', correct: true },
+                { question_id: qstn_ids[1], body: 'Yes' },
+                { question_id: qstn_ids[2], body: 'whale', correct: true },
+                { question_id: qstn_ids[2], body: 'elephant' },
+                { question_id: qstn_ids[2], body: 'zebra' },
+                { question_id: qstn_ids[3], body: '5', correct: true },
+                { question_id: qstn_ids[3], body: '23' },
+                { question_id: qstn_ids[4], body: '12', correct: true },
+                { question_id: qstn_ids[4], body: '26' },
+                { question_id: qstn_ids[4], body: '8' }])
 
-Answer.delete_all
-Answer.create([{questions_id: 1, body: 'No', correct: true},
-               {questions_id: 1, body: 'Yes'},
-               {questions_id: 2, body: 'No', correct: true},
-               {questions_id: 2, body: 'Yes'},
-               {questions_id: 3, body: 'whale', correct: true},
-               {questions_id: 3, body: 'elephant'},
-               {questions_id: 3, body: 'zebra'},
-               {questions_id: 4, body: '5', correct: true},
-               {questions_id: 4, body: '23'},
-               {questions_id: 5, body: '12', correct: true},
-               {questions_id: 5, body: '26'},
-               {questions_id: 5, body: '8'}])
+user_ids = User.create!([{ name: 'Bobby', login: 'megatron',
+                           password: 'bobby123' },
+                         { name: 'Mary', login: 'mar_1',
+                           password: 'mary2010' },
+                         { name: 'John White', login: 'the_best_teacher',
+                           password: 'f8rnkallks8' }]).map(&:id)
 
-
-User.delete_all
-User.create([{id: 1, name: 'Bobby', login: 'megatron', password: 'bobby123'},
-             {id: 2, name: 'Mary', login: 'mar_1', password: 'mary2010'}, 
-             {id: 3, name: 'John White', login: 'the_best_teacher', password: 'f8rnkallks8'}])
-
-UserProfile.delete_all
-UserProfile.create([{user_id: 1, test_id: 1},{user_id: 1, test_id: 3},
-                    {user_id: 1, test_id: 4},{user_id: 2, test_id: 2},
-                    {user_id: 2, test_id: 3},{user_id: 2, test_id: 4}])
+TestPassage.create!([{ user_id: user_ids[0], test_id: test_ids[0] },
+                     { user_id: user_ids[0], test_id: test_ids[2] },
+                     { user_id: user_ids[0], test_id: test_ids[3] },
+                     { user_id: user_ids[1], test_id: test_ids[1] },
+                     { user_id: user_ids[1], test_id: test_ids[2] },
+                     { user_id: user_ids[1], test_id: test_ids[3] }])
