4dec11e66172c72dbe28c6b3dca3e668433ae91c
Dmitrii <veter.666@list.ru>
2018-09-04 23:00:06 +1000

    Исправление замечаний ActiveRecord v2

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,11 @@
 class Test < ApplicationRecord
+  
   belongs_to :category
-  has_many :test_passage 
+  has_many :test_passages 
+  
   def self.by_category(category)
-    select(:title).joins(:category).where(categories: {title: category}).order(title: :desc)    
+    select(:title).joins(:category).where(categories: {title: category}).
+                   order(title: :desc).pluck(:title)    
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,41 +13,41 @@ categories = Category.create!([{ title: 'biology' },
                                { title: 'mathematics' },
                                { title: 'physics' }])
 
-test_ids = Test.create!([{ title: 'botany', level: 1, category: categories[0] },
+tests = Test.create!([{ title: 'botany', level: 1, category: categories[0] },
                          { title: 'zoology', level: 2, category: categories[0] },
                          { title: 'mechanics', level: 3, category: categories[3] },
                          { title: 'optics', level: 2, category: categories[3] },
-                         { title: 'arithmetic', level: 1, category: categories[2] }]).map(&:id)
+                         { title: 'arithmetic', level: 1, category: categories[2] }])
 
-qstn_ids = Question.create!([{ body: 'Is a rose a tree?', test_id: test_ids[0] },
-                             { body: 'Is a beetle a mammal?', test_id: test_ids[1] },
-                             { body: 'Who is the largest animal?', test_id: test_ids[1] },
-                             { body: '2 + 3 =', test_id: test_ids[4] },
-                             { body: '2 * 6 =', test_id: test_ids[4] }]).map(&:id)
+questions = Question.create!([{ body: 'Is a rose a tree?', test_id: tests[0].id },
+                             { body: 'Is a beetle a mammal?', test_id: tests[1].id },
+                             { body: 'Who is the largest animal?', test_id: tests[1].id },
+                             { body: '2 + 3 =', test_id: tests[4] },
+                             { body: '2 * 6 =', test_id: tests[4] }])
 
-Answer.create!([{ question_id: qstn_ids[0], body: 'No', correct: true },
-                { question_id: qstn_ids[0], body: 'Yes' },
-                { question_id: qstn_ids[1], body: 'No', correct: true },
-                { question_id: qstn_ids[1], body: 'Yes' },
-                { question_id: qstn_ids[2], body: 'whale', correct: true },
-                { question_id: qstn_ids[2], body: 'elephant' },
-                { question_id: qstn_ids[2], body: 'zebra' },
-                { question_id: qstn_ids[3], body: '5', correct: true },
-                { question_id: qstn_ids[3], body: '23' },
-                { question_id: qstn_ids[4], body: '12', correct: true },
-                { question_id: qstn_ids[4], body: '26' },
-                { question_id: qstn_ids[4], body: '8' }])
+Answer.create!([{ question_id: questions[0].id, body: 'No', correct: true },
+                { question_id: questions[0].id, body: 'Yes' },
+                { question_id: questions[1].id, body: 'No', correct: true },
+                { question_id: questions[1].id, body: 'Yes' },
+                { question_id: questions[2].id, body: 'whale', correct: true },
+                { question_id: questions[2].id, body: 'elephant' },
+                { question_id: questions[2].id, body: 'zebra' },
+                { question_id: questions[3].id, body: '5', correct: true },
+                { question_id: questions[3].id, body: '23' },
+                { question_id: questions[4].id, body: '12', correct: true },
+                { question_id: questions[4].id, body: '26' },
+                { question_id: questions[4].id, body: '8' }])
 
-user_ids = User.create!([{ name: 'Bobby', login: 'megatron',
+users = User.create!([{ name: 'Bobby', login: 'megatron',
                            password: 'bobby123' },
                          { name: 'Mary', login: 'mar_1',
                            password: 'mary2010' },
                          { name: 'John White', login: 'the_best_teacher',
-                           password: 'f8rnkallks8' }]).map(&:id)
+                           password: 'f8rnkallks8' }])
 
-TestPassage.create!([{ user_id: user_ids[0], test_id: test_ids[0] },
-                     { user_id: user_ids[0], test_id: test_ids[2] },
-                     { user_id: user_ids[0], test_id: test_ids[3] },
-                     { user_id: user_ids[1], test_id: test_ids[1] },
-                     { user_id: user_ids[1], test_id: test_ids[2] },
-                     { user_id: user_ids[1], test_id: test_ids[3] }])
+TestPassage.create!([{ user_id: users[0].id, test_id: tests[0].id },
+                     { user_id: users[0].id, test_id: tests[2].id },
+                     { user_id: users[0].id, test_id: tests[3].id },
+                     { user_id: users[1].id, test_id: tests[1].id },
+                     { user_id: users[1].id, test_id: tests[2].id },
+                     { user_id: users[1].id, test_id: tests[3].id }])
