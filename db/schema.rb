c164d411180dade727efa20dfc58bd7f87680a88
Dmitrii <veter.666@list.ru>
2018-09-06 17:37:25 +1000

    Часть 5: Ассоциации моделей, исправление замечаний

--- db/schema.rb
+++ db/schema.rb
@@ -50,9 +50,9 @@ ActiveRecord::Schema.define(version: 2018_09_05_082426) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
-    t.integer "user_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
-    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
