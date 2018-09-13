9fb08e0ab2f2743ca2e4aed70c450d1a921b4417
Dmitrii <veter.666@list.ru>
2018-09-13 23:59:42 +1000

    Часть 7: Исправление замечаний

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   
-  before_action :find_test, only: :index
+  before_action :find_test, only: %i[index new create]
   before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -18,32 +18,32 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = Question.new
-    question.test_id = params[:question][:test_id].to_i
-    question.body = params[:question][:body]
-    question.save!
-
+    @test.questions.build(question_params).save!
     render plain: 'Question created'    
   end
 
   def destroy
     @question.destroy!
-
     render plain: 'Question deleted'  
   end
 
   private
 
+  def question_params 
+    { test_id: @test.id,
+      body: params[:question][:body] }
+  end  
+
   def find_test
-    @test = Test.find_by_id(params[:test_id])
+    @test = Test.find(params[:test_id])
   end  
 
   def find_question
     @question = Question.find(params[:id])    
   end
 
-  def rescue_with_question_not_found
-    render plain: 'Question was not found'  
+  def rescue_with_question_not_found(exception)
+    render plain: "#{exception.model} whis id #{exception.id} not found."  
   end
   
 end
