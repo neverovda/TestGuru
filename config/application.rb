2b982ab8841dc13464924ff36682366b9f459755
Dmitrii <veter.666@list.ru>
2018-10-05 14:48:54 +1000

    Часть 12: Локализация Rails

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,12 @@ module TestGuru
     config.load_defaults 5.2
 
     config.time_zone = 'Vladivostok'
-    # config.i18n.default_locale = :ru
+    # config.time_zone = 'Moscow'
+    config.i18n.default_locale = :ru
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # the framework and any gems in.i18n your application.
   end
 end
