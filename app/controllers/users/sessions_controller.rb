412208b89f7489fe7dd5ee1d17ed06e8b24623b1
Dmitrii <veter.666@list.ru>
2018-10-08 18:39:39 +1000

    Часть 12: задание

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   
   def create
     super
-    flash[:notice] = "Welcome, #{current_user.first_name} #{current_user.last_name}"     
+    flash[:notice] = "#{t('.welcome')}, #{current_user.first_name} #{current_user.last_name}"
   end   
   
 end
