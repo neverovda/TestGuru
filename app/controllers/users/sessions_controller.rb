8a4102012b2ceb479dbfdd9dfce241b9ba80b838
Dmitrii <veter.666@list.ru>
2018-10-09 12:44:59 +1000

    Часть 12: Не большое замечение (2)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,8 +2,8 @@ class Users::SessionsController < Devise::SessionsController
   
   def create
     super
-    flash[:notice] = t('.welcome', first_name: current_user.first_name,
-                                   last_name: current_user.last_name)
+    flash[:notice] = t('.welcome', 
+                       full_name: "#{current_user.first_name} #{current_user.last_name}")
   end   
   
 end
