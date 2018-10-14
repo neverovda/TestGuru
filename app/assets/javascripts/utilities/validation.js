bbf4931424f39b7e9620de67a21e3831d84e6a3e
Dmitrii <veter.666@list.ru>
2018-10-15 09:23:59 +1000

    Часть 15: добавлены ';'

--- app/assets/javascripts/utilities/validation.js
+++ app/assets/javascripts/utilities/validation.js
@@ -1,36 +1,36 @@
 document.addEventListener('turbolinks:load', function() {
-  var form = document.querySelector('.registration')
-  if (form) { passwordConfirmation() }
+  var form = document.querySelector('.registration');
+  if (form) { passwordConfirmation() };
 })
 
 function passwordConfirmation() {
 
-  passField = document.getElementById('user_password')
-  passConfirmField = document.getElementById('user_password_confirmation')
-  alertDirrerence = document.querySelector('.password-dirrerence')
+  passField = document.getElementById('user_password');
+  passConfirmField = document.getElementById('user_password_confirmation');
+  alertDirrerence = document.querySelector('.password-dirrerence');
 
 
-  passField.addEventListener('keyup', checkDifference)
-  passConfirmField.addEventListener('keyup', checkDifference)
+  passField.addEventListener('keyup', checkDifference);
+  passConfirmField.addEventListener('keyup', checkDifference);
 
   function checkDifference() {
     
-    passConfirmation = passConfirmField.value
-    password = passField.value
+    passConfirmation = passConfirmField.value;
+    password = passField.value;
 
     if (passConfirmation == '' ||  password == passConfirmation) { 
-      hideAlert() 
-      return
+      hideAlert();
+      return;
       }
     showAlert() 
   }
 
   function hideAlert() {
-    alertDirrerence.classList.add('hide')
+    alertDirrerence.classList.add('hide');
   }
 
   function showAlert() {
-    alertDirrerence.classList.remove('hide')
+    alertDirrerence.classList.remove('hide');
   }
 
 }
