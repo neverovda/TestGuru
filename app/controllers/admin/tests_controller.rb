c2cf685a03b8e9649b4502328ade8eca24b0c2d7
Dmitrii <veter.666@list.ru>
2018-10-15 19:12:06 +1000

    Часть 16: Data Attributes, код из лекции

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,9 +1,10 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :find_test, only: %i[show update destroy start]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show update destroy start update_inline]
 
   def index
-    @tests =Test.all
+
   end
 
   def show    
@@ -35,12 +36,24 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+   if @test.update(test_params)
+      redirect_to admin_tests_path 
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
     redirect_to admin_tests_path
   end
 
   private
+
+  def set_tests
+    @tests =Test.all
+  end
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
